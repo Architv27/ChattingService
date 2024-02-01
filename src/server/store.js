c4402969fd5669af3c3b5c73abe915c255b1f2df
Architv27 <archit.sfu@gmail.com>
2024-02-01 15:11:13 -0800

    slicer removed

--- src/server/store.js
+++
@@ -1,9 +0,0 @@
-// store.js
-import { configureStore } from '@reduxjs/toolkit';
-import roomsReducer from './Slicers/roomSlicer.js';
-
-export const store = configureStore({
-  reducer: {
-    rooms: roomsReducer,
-  },
-});
